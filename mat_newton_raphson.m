8de113d5f729326d9b0f4e8aa61dbad1e71185ef
jonathanantunes <31494131+jonathanantunes@users.noreply.github.com>
2017-12-08 19:17:49 -0200

    teste

--- mat_newton_raphson.m
+++ mat_newton_raphson.m
@@ -2,27 +2,27 @@ function xn1=mat_newton_raphson(F,J,xn,N,e1,e2,e3)
  % Metodo de Newton Raphson
  %
  % xn1=mat_newton_raphson(F,J,xn,N,e1,e2,e3)
- 
+ p=xn;
   for i=1:N
       h=J(xn)\(-F(xn));
       
       xn1=xn+h;
-     
-      xn=xn1
- 
-  
-  
-  
-  end
+    
       
-      if (abs(f(xn1))<=e1)       
+      if norm(F(xn1))<=e1      
            break 
-       end
+      end
+       
+      if norm(xn1-p)<=e2     
              
            break
       end
-      if abs((xn1-xn)/xn1)<=e3         
+      if abs((xn1-p)/xn1)<=e3         
            
            break
       end
+      
+      xn=xn1;
+      p=xn;
+  end
  
